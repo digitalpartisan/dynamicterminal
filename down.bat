3e55d9cbb104005f2bbb79013e02de5050e0198c
digitalpartisan <digital.partisan.86@gmail.com>
2017-01-20 22:12:32 -0800

    added .bat files

---
+++ down.bat
@@ -0,0 +1,2 @@
+del /q "K:\Steam\steamapps\common\Fallout 4\Data\DynamicTerminalTesting.esp"
+rmdir /s /q "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\DTT" "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\DTT"
---
+++ up.bat
@@ -0,0 +1,3 @@
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\DynamicTerminalTesting.esp" "K:\FO4 Mods\DynamicTerminalTesting\Data\DynamicTerminalTesting.esp"
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\DTT" "K:\FO4 Mods\DynamicTerminalTesting\Data\Scripts\DTT"
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\DTT" "K:\FO4 Mods\DynamicTerminalTesting\Data\Scripts\Source\User\DTT"
