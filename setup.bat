35c85b00ea9630c1947ec9194e13df4a7c3307a5
Joel <joel.flake@gmail.com>
2017-01-20 21:36:25 -0800

    added packed files and .bat helpers

---
+++ setup.bat
@@ -0,0 +1,1 @@
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\DynamicTerminal.esp" "K:\FO4 Mods\DynamicTerminal\Data\DynamicTerminal.esp"
---
+++ teardown.bat
@@ -0,0 +1,3 @@
+del /q "K:\Steam\steamapps\common\Fallout 4\Data\DynamicTerminal - Main.ba2"
+del /q "K:\Steam\steamapps\common\Fallout 4\Data\DynamicTerminal.esp"
+rmdir /s /q "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\DynamicTerminal" "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\DynamicTerminal"
