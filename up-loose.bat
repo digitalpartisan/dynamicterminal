23602b14d46c21e920cd980f1de698481f2dc152
digitalpartisan <digital.partisan.86@gmail.com>
2017-01-20 22:08:35 -0800

    made .bat files match pattern developed to support more complicated mods

--- up-loose.bat
+++ up-loose.bat
@@ -1,3 +1,2 @@
-del /q "K:\Steam\steamapps\common\Fallout 4\Data\DynamicTerminal - Main.ba2"
 mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\DynamicTerminal" "K:\FO4 Mods\DynamicTerminal\Data\Scripts\DynamicTerminal"
 mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\DynamicTerminal" "K:\FO4 Mods\DynamicTerminal\Data\Scripts\Source\User\DynamicTerminal"
