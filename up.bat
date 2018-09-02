a0f28bcc2c171994ff11caabae0ce9f5f40305c1
digitalpartisan <digital.partisan.86@gmail.com>
2018-09-02 15:21:05 -0700

    Fixed a bug where the old testing package was still referenced in a bat file

--- up.bat
+++ up.bat
@@ -1,2 +1,2 @@
 mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\DynamicTerminal.esp" "K:\FO4 Mods\DynamicTerminal\Data\DynamicTerminal.esp"
-mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\DynamicTerminalTesting.esp" "K:\FO4 Mods\DynamicTerminalTesting\Data\DynamicTerminalTesting.esp"
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\DynamicTerminalTesting.esp" "K:\FO4 Mods\DynamicTerminal\Data\DynamicTerminalTesting.esp"
